f08b7891f4eabf57e2ddf1307c6151e25edcf9d5
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2013-05-09 13:05:07 -0600

    clear up for release

--- nkLandscape.py
+++ nkLandscape.py
@@ -1,10 +1,7 @@
 # generate NK-landscapes instances
 ## the encoding is in right to left fashion
 ## 00-0, 01-1, 10-2, 11-3 
-import WalshAnalysis as wal
 import random
-import numpy as np
-import matplotlib.pyplot as plt
 import math
 
 class NKLandscape:
@@ -58,75 +55,7 @@ class NKLandscape:
             #print 'i', i, 'index in func', int(interStr,2), 'interStr', interStr
             sum = sum + self.func[i][int(interStr,2)]
         return sum
-    def WalCof(self):
-        """ compute the Walsh coefficients """
-        subW = [] # subW is a N*2^K matrix
-        for i in range(self.n):
-            """ 1. Compute coefficients for each sub-functions """
-            subWone = wal.computeW(self.Kbits, self.func[i])
-            subW.append(subWone)
-        w = np.zeros(math.pow(2,self.n))
-        for i in range(int(math.pow(2,self.n))): # for every candidate solution
-            iStr = bin(i)
-            iStr = iStr[2:]
-            if len(iStr) < self.n:
-                iStr = (self.n - len(iStr))*'0' + iStr
-            for j in range(self.n): # for every sub-function
-                maskJ = self.neighs[j][:]
-                maskJ.append(j)
-                maskJ.sort()
-                # pack iStr to a (k+1) length one
-                maskStr = [iStr[k] for k in maskJ]
-                maskStr = ''.join(maskStr)
-                occurOneBit = self.indexOneBit(iStr)
-                if self.checkInclude(occurOneBit, maskJ) == True :
-                    extractBit = maskStr
-                    w[i] = w[i] + subW[j][int(extractBit, 2)]
-        return w
-    def WalCofLinear(self):
-        """ compute the Walsh coefficients in a linear time """
-        subW = [] # subW is a N*2^K matrix
-        for i in range(self.n):
-            """ 1. Compute coefficients for each sub-functions """
-            subWone = wal.computeW(self.Kbits, self.func[i])
-            subW.append(subWone)
-#        print 'subW', subW
-        w = np.zeros(math.pow(2,self.n))
-        for i in range(self.n): # i: index of subfunction
-            interBits = self.neighs[i][:]
-            interBits.append(i)
-            interBits.sort()
-#            print 'interBits',interBits
-            for j in range(int(math.pow(2, self.k+1))): # j: index of substrings
-                indexW = self.composeFullStr(i, j, interBits, self.n)
-                w[indexW] = w[indexW] + subW[i][j]
-#                print 'i',i,'j', j, 'w', w
-        return w
-
-    def composeFullStr(self, i, j, interBits, n):
-        subStr = bin(j)
-        subStr = subStr[2:]
-        if len(subStr) < self.k+1:
-            subStr = '0'*(self.k+1-len(subStr)) + subStr
-#        print 'subStr', subStr
-        indexSubOneBit = self.indexOneBit(subStr)
-#        print 'indexSubOneBit', indexSubOneBit 
-        iStr = ['0']*n
-        for k in range(len(indexSubOneBit)):
-            iStr[interBits[indexSubOneBit[k]]] = subStr[indexSubOneBit[k]]
-        iStr = ''.join(iStr)
-#        print 'iStr', iStr
-        return int(iStr, 2)
-
-    def checkInclude(self, occurOneBit, mask):
-        for i in range(len(occurOneBit)):
-            if occurOneBit[i] not in mask:
-                return False
-        return True
-
-    def indexOneBit(self, iStr):
-        range1 = range(len(iStr))
-        return [ i for i in range1 if iStr[i] == '1']
+    
             
 def genSeqBits(n):
     bitStr = []
@@ -138,7 +67,8 @@ def genSeqBits(n):
        bitStr.append(bit)
     return bitStr
 
-#model = NKLandscape(5,2)
-#print model.getNeigh()
-#print model.getFunc()
-#print model.compFit('10001001010101010100')
+# testing purpose
+# model = NKLandscape(5,2)
+# print model.getNeigh()
+# print model.getFunc()
+# print model.compFit('10001001010101010100')
